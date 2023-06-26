45feeaa88c6bdeaba16848ed19e00cb07d2d159d
Alexander Royal <alexander.de.royal@gmail.com>
2023-06-26 16:48:21 -0500

    sum of the digits completed

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
 public class Main {
 
-    public static int sumDigits(int number) {
-        if(number < 0){
-            return -1;
-        } else {
+//    public static void main(String[] args) {
+//        System.out.println("The sum of the digits in number is 1234 is " + sumDigits(1234));
+//    }
+//
+//    public static int sumDigits(int number) {
+//
+//        if(number < 0){
+//            return -1;
+//        }
+//        int sum = 0;
+//        while(number > 9){
+//            sum += (number % 10);
+//            number = number / 10;
+//        }
+//        sum += number;
+//        return sum;
+//    }
 
-        }
-    }
+
 //    public static void main(String[] args) {
 //
 //        int number = 4;
