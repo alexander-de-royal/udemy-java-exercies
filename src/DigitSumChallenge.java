4348c9379002edafa7cd0d3979d9ad3666912180
Alexander Royal <alexander.de.royal@gmail.com>
2023-06-26 13:24:02 -0500

    odd or even sum completed

---
+++ src/DigitSumChallenge.java
@@ -0,0 +1,9 @@
+public class DigitSumChallenge {
+    public static int sumDigits(int number) {
+        if(number < 0){
+            return -1;
+        } else {
+
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,67 @@
 public class Main {
 
+    public static int sumDigits(int number) {
+        if(number < 0){
+            return -1;
+        } else {
+
+        }
+    }
+//    public static void main(String[] args) {
+//
+//        int number = 4;
+//        int finishNumber = 20;
+//        int evenCount = 0;
+//        int oddCount = 0;
+//
+//        while(number <= finishNumber){
+//            number++;
+//            if(!isEvenNumber(number)){
+//                oddCount++;
+//                continue;
+//            }
+//            System.out.println("Even Number " + number);
+//            evenCount++;
+//            if(evenCount >= 5){
+//                break;
+//            }
+//        }
+//        System.out.println("Total odd numbers found = " + oddCount);
+//        System.out.println("Total even numbers found = " + evenCount);
+//    }
+//
+//    public static boolean isEvenNumber(int number) {
+//        if ((number % 2) == 0){
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//    public static void main(String[] args) {
+//        int number = 0;
+//        while(number < 50){
+//            number += 5;
+//            if(number % 25 == 0){
+//                continue;
+//            }
+//            System.out.println(number + "_");
+//        }
+//    }
+//    public static void main(String[] args) {
+//
+//        for(int i = 1; i <=5; i++){
+//            System.out.println(i);
+//        }
+//        int j = 1;
+//        boolean isReady = false;
+//        do{
+//            if(j > 5){
+//                break;
+//            }
+//            System.out.println(j);
+//            j++;
+//        } while (isReady);
+//    }
 //    public static void main(String[] args) {
 //        int countOfMatches = 0;
 //        int sumOfMatches = 0;
